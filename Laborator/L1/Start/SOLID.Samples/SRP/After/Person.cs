67572c55f7765473818cc006f64477045e307f6e
adrianB3 <balanescuadrian71@gmail.com>
2019-10-09 14:50:04 +0300

    srp completed

---
+++ Laborator/L1/Start/SOLID.Samples/SRP/After/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SOLID.Samples.SRP.After
+{
+	public class Person
+	{
+        private Account _personAccount;
+		public Person(string name, decimal accountBalance)
+		{
+			Name = name;
+            _personAccount = new Account(accountBalance);
+		}
+
+		public string Name { get; private set; }
+
+	}
+}
