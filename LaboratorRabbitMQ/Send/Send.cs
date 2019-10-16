9fb515cbbde87bd4bfd8e52b6e4450a1e2107c40
bogdansebi <bogdansebastian12@yahoo.com>
2019-10-16 17:33:18 +0300

    Laboratorul3

---
+++ LaboratorRabbitMQ/Send/Send.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { Uri=new Uri ("amqp://qgamjvrp:b5uAFWenQCSyWnNZ6DsSeA7fOYd6zir4@hedgehog.rmq.cloudamqp.com/qgamjvrp") };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" Tarau Adrian", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
