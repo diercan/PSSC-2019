b17985f2c9f292a78d0459d644ec2f29eb5c0f0f
Andricsak Alexandru <alex.andricsak@gmail.com>
2019-11-11 13:43:11 +0200

    Added empty test project

---
+++ Andricsak/Proiect/GameRentWeb/TestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
