904de9875e774171c4572b2dfa6084ce31d7f8bd
georgiana-cretea <cretea2g@yahoo.com>
2020-01-03 19:49:30 +0200

    aplicatie

---
+++ Cretea Georgiana/Proiect/PerfumeShop1/Controllers/MeniuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PerfumeShop1.Controllers
+{
+    public class MeniuController : Controller
+    {
+        // GET: Meniu
+        public ActionResult Meniu()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cretea Georgiana/Proiect/PerfumeShop1/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PerfumeShop1.Models;
+
+namespace PerfumeShop1.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add(int id=0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult Add(User userModel)
+        {
+            using(DbModels dbModel = new DbModels())
+            {
+                if(dbModel.Users.Any(x=>x.Username==userModel.Username))
+                {
+                    ViewBag.DuplicateMessage = "Username already exist";
+                    return View("Add", userModel);
+                }
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful";
+            return View("Add", new User());
+        }
+    }
+}
