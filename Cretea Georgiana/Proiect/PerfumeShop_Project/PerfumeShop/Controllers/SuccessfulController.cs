6d1ab2d524e9f4583b686a58342ac4c9e65300c5
georgiana-cretea <cretea2g@yahoo.com>
2020-01-07 13:20:58 +0200

    update

---
+++ Cretea Georgiana/Proiect/PerfumeShop_Project/PerfumeShop/Controllers/SuccessfulController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PerfumeShop.Services;
+
+namespace PerfumeShop.Controllers
+{
+    public class SuccessfulController : Controller
+    {
+        public IActionResult Index()
+        {
+            Send snd = new Send("Congratulations! Your order has been confirmed");
+            return View();
+        }
+    }
+}
---
+++ Cretea Georgiana/Proiect/PerfumeShop_Project/ReceiveOrder/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiveOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { Uri = new Uri("amqp://sjlxrrww:VBdMZA0ABgfZ9jNeXCiBPjdWpdUrFkUe@dove.rmq.cloudamqp.com/sjlxrrww") };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
