0cea121fcc44bd986360f84d696146a53e3561ce
Bogdan Sebastian <bogdansebastian12@yahoo.com>
2020-01-08 11:04:01 +0200

    mesaj

---
+++ Bogdan_Sebastian/Project/Coada/Coada/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coada
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;user=root;database=Spalatorie;password=;");
+        }
+        public DbSet<Spalatorie> Spalatorii { get; set; }
+    }
+}
---
+++ Bogdan_Sebastian/Project/Coada/Coada/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Coada
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ApplicationContext();
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://zkchlziu:G_s895kf9DF-Z9kWuFNkyOVHH09Jgu0I@barnacle.rmq.cloudamqp.com/zkchlziu");
+            factory.UserName = "zkchlziu";
+            factory.Password = "G_s895kf9DF-Z9kWuFNkyOVHH09Jgu0I";
+            var _channel = factory.CreateConnection().CreateModel();
+            _channel.QueueDeclare(queue: "createSpalatorie",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                string message = Encoding.UTF8.GetString(body);
+                SpalatorieDto o = JsonConvert.DeserializeObject<SpalatorieDto>(message);
+                Spalatorie newSpalatorie = new Spalatorie
+                {
+                    Name = o.Name,
+                    Number = o.Number,
+                    Price = o.Price,
+                    PhotoPath = o.PhotoPath
+                };
+                Spalatorie[] last = context.Spalatorii.ToArrayAsync().Result;
+                if(last.Length != 0)
+                {
+                    newSpalatorie.Id = last[last.Length - 1].Id + 1;
+                }
+                context.Spalatorii.Add(newSpalatorie);
+                context.SaveChanges();
+                Console.WriteLine(message);
+            };
+            _channel.BasicConsume(queue: "createSpalatorie",
+                autoAck: true,
+                consumer: consumer);
+        }
+    }
+}
